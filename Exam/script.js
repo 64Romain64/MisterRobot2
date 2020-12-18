19a9ea46c0eccc3d4bf51417526f45c59f91cca7
64Romaine64 <g.romain64@gmail.com>
2020-12-18 17:02:36 +0100

    Version 4.0

--- Exam/script.js
+++ Exam/script.js
@@ -1,8 +1,8 @@
 let byeBye = document.getElementById("rom1");
 
 function changeClass(){
-    alert("Bye Bye");
-    document.getElementById('rom1').classList.add('leave');
+    // alert("Bye Bye");
+    byeBye.classList.add('leave');
 };
 
 byeBye.addEventListener("click", changeClass);
